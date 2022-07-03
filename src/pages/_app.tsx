530c9a600af7e19c510c7d9053eaf1e6b671dfd5
Václav Piroutek <vasek.pirou@gmail.com>
2022-07-03 15:18:35 +0200

    added trpc and react query dev tools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,29 @@
-import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../chakra/config";
+import { withTRPC } from "@trpc/next";
+import { AppType } from "next/dist/shared/lib/utils";
+import { AppRouter } from "./api/trpc/[trpc]";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      {process.env.NODE_ENV !== "production" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
     </ChakraProvider>
   );
 };
 
-export default MyApp;
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : "http://localhost:3000/api/trpc";
+    return {
+      url,
+    };
+  },
+  ssr: true,
+})(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import { NextPage } from "next/types";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  const hello = trpc.useQuery(["hello", { text: "svete" }]);
+  const utils = trpc.useContext();
+
+  return (
+    <div>
+      <p>{hello.data?.greeting}</p>
+      <button
+        onClick={() => {
+          utils.invalidateQueries("hello");
+        }}
+      >
+        random
+      </button>
+    </div>
+  );
 };
 
 export default Home;
